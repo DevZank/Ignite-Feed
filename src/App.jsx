4e0da31d0aef5679db5ed5fc9fb27b51ae42cdd8
Isaac Martins <111926496+DevZank@users.noreply.github.com>
2024-01-24 14:37:00 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Post } from "./components/Post"
+import { Header } from "./components/header"
+import { Sidebar } from "./components/Sidebar"
+import styles from "./App.module.css"
+import "./global.css"
+
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Isaac Martins"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis deserunt quisquam maxime, aspernatur dolore incidunt repudiandae libero beatae quasi tempore, a velit impedit dolorem aperiam esse voluptatem alias doloribus!"
+          />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+
