917e439503d7654555b55c764f1551db97d0ff1e
Isaac Martins <111926496+DevZank@users.noreply.github.com>
2024-01-24 14:40:19 -0300

    Fixing Commit error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Post } from "./components/Post"
-import { Header } from "./components/header"
+import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
 import styles from "./App.module.css"
 import "./global.css"
